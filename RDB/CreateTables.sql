31893d3eb89bb5cd8b3b9920207f1d6ea18a3ccd
Trent Harris <tdh00010@mix.wvu.edu>
2026-09-09 17:54:32 -0400

    Created Room and RoomAvailabilty tables

--- RDB/CreateTables.sql
+++ RDB/CreateTables.sql
@@ -1,4 +1,4 @@
--- in your master database
+/*-- in your master database
 
 CREATE LOGIN NandaSurendra
 
@@ -11,3 +11,26 @@ CREATE USER NandaSurendra
 FOR LOGIN NandaSurendra;
 
 ALTER ROLE db_owner ADD MEMBER NandaSurendra;
+*/
+Drop table if exists Room;
+drop table if exists RoomAvailability;
+
+go
+
+create table Room
+(
+    RoomID int identity(1,1) primary key,
+    RoomName varchar(10) not null,
+    Whiteboard bit not null,
+ );
+
+ go
+
+ create table RoomAvailability
+ (
+    RoomAvailabilityID int identity(1,1) primary key,
+    RoomID int not null,
+    AvailableDate date not null,
+    AvailableStartTime time not null,
+    constraint FK_RoomAvailability_Room foreign key (RoomID) references Room(RoomID)
+ );
